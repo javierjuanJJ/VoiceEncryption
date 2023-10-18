8856b061206ea067b38bf0256565d1b9c16f16c8
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-10-18 13:22:04 +0200

    Commit 4

--- app/src/main/java/com/example/voiceencryption/MainActivity.java
+++ app/src/main/java/com/example/voiceencryption/MainActivity.java
@@ -22,11 +22,14 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, TextToSpeech.OnInitListener {
 
+    public static final File NAME_HELLO = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/hello-5.mp3");
     private Button btnPlayAudio, btnStopAudio, btnPlayAudio2;
     private MediaPlayer player;
     private File file;
@@ -73,13 +76,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btnPlayAudio:
+                Log.i("SuccesfullEncrypt","startSound();");
                 startSound();
                 break;
             case R.id.btnStopAudio:
-                player.stop();
+
                 break;
             case R.id.btnPlayAudio2:
-                textToSpeech.speak(encrypt.substring(2,30), TextToSpeech.QUEUE_FLUSH, null);
+                textToSpeech.speak(encrypt.substring(2, 30), TextToSpeech.QUEUE_FLUSH, null);
                 break;
             default:
                 // throw new IllegalStateException("Unexpected value: " + idView);
@@ -89,28 +93,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void startSound() {
         try (
                 AssetFileDescriptor assetFileDescriptor = getResources().getAssets().openFd("mirage.mp3");
-                FileOutputStream os = new FileOutputStream(Environment.getExternalStorageDirectory() + "/hello-5.mp3", true);
+                FileOutputStream os = new FileOutputStream(NAME_HELLO, false);
         ) {
+            file = Utity.createFileFromInputStream(assetFileDescriptor.createInputStream(), NAME_HELLO);
 
-            file = new File(Environment.getExternalStorageDirectory() + "/mirage.mp3");
             byte[] bytes = FileUtils.readFileToByteArray(file);
+
             String encoded = Base64.encodeToString(bytes, 0);
 
             CipherEncrypt(encoded, 5);
             byte[] decoded = Base64.decode(encoded, 0);
             os.write(decoded);
 
-            if (assetFileDescriptor != null){
+            if (assetFileDescriptor != null) {
                 player.setDataSource(file.toString());
                 player.prepare();
                 player.start();
+                player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        finishSong(mediaPlayer);
+                    }
+                });
             }
+            Log.i("SuccesfullEncrypt4", String.valueOf(NAME_HELLO));
+
+
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            Log.e("ExceptionError", IOException.class.getName() + " " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            Log.e("ExceptionError", Exception.class.getName() + " " + e.getMessage());
+            e.printStackTrace();
         }
-        catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    }
+
+    private void finishSong(MediaPlayer mediaPlayer) {
+        mediaPlayer.stop();
+        player = new MediaPlayer();
     }
 
     public void CipherEncrypt(String text, int s) {
@@ -121,17 +141,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             result.append(Character.isUpperCase(text.charAt(counter)) ? (char) (((int) text.charAt(counter) + s - 65) % 26 + 65) : (char) (((int) text.charAt(counter) + s - 97) % 26 + 97));
         }
         encrypt = Utity.getOnlyString(result.toString());
-        try(FileOutputStream os = new FileOutputStream(Environment.getExternalStorageDirectory() + "/jirage.mp3", true);) {
+        try (FileOutputStream os = new FileOutputStream(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/jirage.mp3", true);) {
             byte[] decoded = Base64.decode(result.toString(), 0);
             os.write(decoded);
-        }catch (Exception e){
-            Log.e("ExceptionError", Objects.requireNonNull(e.getMessage()));
+        } catch (Exception e) {
+            Log.e("ExceptionError", Exception.class.getName() + " " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
     @Override
     public void onInit(int i) {
-        if(i != TextToSpeech.ERROR){
+        if (i != TextToSpeech.ERROR) {
             textToSpeech.setLanguage(Locale.UK);
         }
     }
--- app/src/main/java/com/example/voiceencryption/Utity.java
+++ app/src/main/java/com/example/voiceencryption/Utity.java
@@ -1,5 +1,10 @@
 package com.example.voiceencryption;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,4 +21,21 @@ public class Utity {
         String numDigits = matcher.replaceAll("");
         return numDigits;
     }
+
+    public static File createFileFromInputStream(InputStream inputStream, File f) throws IOException {
+
+        OutputStream outputStream = new FileOutputStream(f);
+        byte buffer[] = new byte[1024];
+        int length = 0;
+
+        while((length=inputStream.read(buffer)) > 0) {
+            outputStream.write(buffer,0,length);
+        }
+
+        outputStream.close();
+        inputStream.close();
+
+        return f;
+    }
+
 }
